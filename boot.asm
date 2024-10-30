127b6999793f63b9a4ebbb8688730e6563242c64
Bishnu Timilsena <timilsenabsnu@gmail.com>
2024-10-30 11:11:40 +0545

    read from the hard disk

--- boot.asm
+++ boot.asm
@@ -1,29 +1,15 @@
 ; created own interrupt
 ORG 0
 BITS 16
+
 _start:
     jmp short start ;jump to the 'start' label to skip the padding bytes
     nop
 
  times 33 db 0 ; reserve 33 bytes for potential BIOS Parameter Block space
 
-
 start:
     jmp 0x7c0: step2 ; this will make code egment start at 0x7c0
-
-handle_zero:
-    mov ah, 0eh
-    mov al, 'A'
-    mov bx, 0x00
-    int 0x10
-    iret ;interrupt finished
-
-handle_one:
-     mov ah, 0eh
-     mov al, 'V'
-     mov bx, 0x00
-     int 0x10
-     iret
 
 step2:
     cli ; clear interrupts
@@ -35,20 +21,24 @@ step2:
     mov sp, 0x7c00
     sti ; enables interrupts
 
-    mov word[ss:0x00], handle_zero
-    mov word[ss:0x02], 0x7c0
-    
-    ;mov ax, 0x00
-    ;div ax
+    mov ah, 2 ; read sector command
+    mov al, 1 ; one sector to read
+    mov ch, 0 ; cylinder low 8 bits
+    mov cl, 2 ; read sector 2
+    mov dh, 0 ; head number
+    mov bx, buffer
+    int 0x13 ; invoke the read command
+    jc error
 
-    mov word[ss:0x04], handle_one
-    mov word[ss:0x06], 0x7c0
-
-    int 1
-
-    mov si, message
+    mov si, buffer
     call print
+
     jmp $
+
+error:
+      mov si, error_message
+      call print
+      jmp $
 
 print:
   mov bx, 0 ;Page number set to 0
@@ -68,7 +58,9 @@ print_char:
   int 0x10 ; BIOS interrupt to display character in AL
   ret
 
-message: db 'Hello World!', 0
+error_message: db 'Failed to load setcor', 0
 
 times 510-($ - $$) db 0
 dw 0xAA55
+
+buffer: 
